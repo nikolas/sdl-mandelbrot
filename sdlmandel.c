3671256183cc87404e9497b007dc8cd2409fcbb6
Nikolas Nyby <nikolas@gnu.org>
2019-03-08 18:40:58 -0500

    Update to SDL 2.0

--- sdlmandel.c
+++ sdlmandel.c
@@ -1,7 +1,7 @@
 #ifdef _MSC_VER
-#include <SDL.h>
+#include <SDL2.h>
 #else
-#include <SDL/SDL.h>
+#include <SDL2/SDL.h>
 #endif /* _MSC_VER */
 
 #include <stdio.h>
@@ -19,34 +19,46 @@
 
 #define ZOOM_FACTOR     4
 
-void sdl_draw_mandelbrot(SDL_Surface *surface, complex double center, double zoom)
+void sdl_draw_mandelbrot(SDL_Window *window, complex double center, double zoom)
 {
+    int width, height;
+    SDL_GetWindowSize(window, &width, &height);
+
     int f,x,y,n;
-    int maxiter = (WIDTH/2) * 0.049715909 * log10(zoom);
+    int maxiter = (width/2) * 0.049715909 * log10(zoom);
     complex double z, c;
     float C;
 
-    static SDL_Rect rects[HEIGHT/FLIPS];
+    SDL_Rect rects[height/FLIPS];
+    SDL_Surface *surface;
+
+    surface = SDL_GetWindowSurface(window);
+
+    if ( surface == NULL )
+    {
+        fprintf(stderr, "Could not get surface : %s\n", SDL_GetError());
+        exit(1);
+    }
 
     fprintf(stderr, "zoom: %f\n", zoom);
-    fprintf(stderr, "center point: %f %+fi\n", creal(center), 
+    fprintf(stderr, "center point: %f %+fi\n", creal(center),
                                               cimag(center) );
     fprintf(stderr, "iterations: %d\n", maxiter);
 
     for (f = 0; f < FLIPS; f++)
     {
-        for  (y = f; y < HEIGHT; y += FLIPS)
+        for  (y = f; y < height; y += FLIPS)
         {
-            for (x = 0; x < WIDTH; x++)
+            for (x = 0; x < width; x++)
             {
                 /* Get the complex poing on gauss space to be calculate */
-                z = c = creal(center) + ((x - (WIDTH/2))/zoom) + 
-                    ((cimag(center) + ((y - (HEIGHT/2))/zoom))*_Complex_I);
+                z = c = creal(center) + ((x - (width/2))/zoom) +
+                    ((cimag(center) + ((y - (height/2))/zoom))*_Complex_I);
 
                 #define X creal(z)
                 #define Y cimag(z)
 
-                /* Check if point lies within the main cardiod or 
+                /* Check if point lies within the main cardiod or
                    in the period-2 buld */
                 if ( (pow(X-.25,2) + pow(Y,2))*(pow(X,2) + (X/2) + pow(Y,2) - .1875) < pow(Y,2)/4 ||
                      pow(X+1,2) + pow(Y,2) < .0625 )
@@ -58,18 +70,26 @@ void sdl_draw_mandelbrot(SDL_Surface *surface, complex double center, double zoo
 
                 C = n - log2f(logf(cabs(z)) / M_LN2 );
 
-                /* Paint the pixel calculated depending on the number 
+                /* Paint the pixel calculated depending on the number
                    of iterations found */
-                ((Uint32*)surface->pixels)[(y*surface->w) + x] = (n >= maxiter)? 0 :
-                    SDL_MapRGB( surface->format,
-                    (1+sin(C*0.27 + 5))*127., (1+cos(C*0.85))*127., (1+sin(C*0.15))*127. );
+                ((Uint32*)surface->pixels)[(y*surface->w) + x] =
+                    (n >= maxiter) ? 0 :
+                    SDL_MapRGB(
+                        surface->format,
+                        (1+sin(C*0.27 + 5))*127.,
+                        (1+cos(C*0.85))*127.,
+                        (1+sin(C*0.15))*127.);
             }
             rects[y/FLIPS].x = 0;
             rects[y/FLIPS].y = y;
-            rects[y/FLIPS].w = WIDTH;
+            rects[y/FLIPS].w = width;
             rects[y/FLIPS].h = 1;
         }
-        SDL_UpdateRects(surface, HEIGHT/FLIPS, rects);
+        SDL_UpdateWindowSurfaceRects(window, rects, height/FLIPS);
+    }
+
+    if (width % FLIPS != 0) {
+        SDL_UpdateWindowSurface(window);
     }
 }
 
@@ -83,13 +103,18 @@ int main(int argc, char **argv)
     }
     atexit(SDL_Quit);
 
-    SDL_Surface *surface;
+    SDL_Window *window;
 
-    surface = SDL_SetVideoMode(WIDTH, HEIGHT, 32, SDL_HWSURFACE);
-    if ( surface == NULL )
+    window = SDL_CreateWindow(
+        "sdlmandel",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        WIDTH, HEIGHT,
+        0);
+
+    if ( window == NULL )
     {
-        fprintf(stderr, "Could not setup screen to resolution %dx%d : %s\n", 
-                WIDTH, HEIGHT, SDL_GetError());
+        fprintf(stderr, "Couldn\'t create window : %s\n",
+                SDL_GetError());
         exit(1);
     }
 
@@ -97,7 +122,7 @@ int main(int argc, char **argv)
     double complex center = START_POS;
     double zoom = START_ZOOM;
 
-    sdl_draw_mandelbrot(surface, center, zoom);
+    sdl_draw_mandelbrot(window, center, zoom);
 
     SDL_Event event;
     while(1)
@@ -113,7 +138,7 @@ int main(int argc, char **argv)
                 {
                     center = START_POS;
                     zoom = START_ZOOM;
-                    sdl_draw_mandelbrot(surface, center, zoom);
+                    sdl_draw_mandelbrot(window, center, zoom);
                 }
                 else if (event.key.keysym.sym == SDLK_ESCAPE)
                 {
@@ -130,7 +155,7 @@ int main(int argc, char **argv)
                 else if (event.button.button == 3)
                     zoom /= ZOOM_FACTOR;
 
-                sdl_draw_mandelbrot(surface, center, zoom);
+                sdl_draw_mandelbrot(window, center, zoom);
                 break;
         }
     }
